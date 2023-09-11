7252f5a40ffdf223fe44961d44f6158dc5a7c6b2
1iki <87522077+1iki@users.noreply.github.com>
2023-09-12 00:44:43 +0700

    Add files via upload

---
+++ ecommerce website/components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>tautan langsung</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> beranda</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> tentang</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> belanja</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> kontak</a>
+      </div>
+
+      <div class="box">
+         <h3>tambahan link</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> mendaftar</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> keranjang</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> pesanan</a>
+      </div>
+
+      <div class="box">
+         <h3>kontak kami</h3>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +62 1212 3456 7890</a>
+         <a href="tel:11122233333"><i class="fas fa-phone"></i> +62 1212 3456 7890</a>
+         <a href="mailto:tester@gmail.com"><i class="fas fa-envelope"></i> tester@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i>Jakarta Selatan, RT.1/RW.7, Pejaten Bar., Ps. Minggu, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12510 </a>
+      </div>
+
+      <div class="box">
+         <h3>ikuti kami</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Putra  Krisna  Fachri</span> | all rights reserved!</div>
+
+</footer>
